af34d5e7a43e9fe162d3f48f79cc52c4df60c237
javieerhg-hub <javieer.hg@gmail.com>
2026-08-02 09:02:24 -0400

    Delete src/main.jsx

--- src/main.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import HowriaAdmin from "./HowriaAdmin.jsx";
-
-ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <HowriaAdmin />
-  </React.StrictMode>
-);
